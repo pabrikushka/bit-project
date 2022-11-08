fff59fbb72106bf83484da36cd68c5c900e319db
Sergei <sergei.zagoskin@microchannel.co.nz>
2022-11-08 16:38:16 +1300

    scroll and camera v1, refactroing

--- src/components/gallery/GalleryWidget.tsx
+++ src/components/gallery/GalleryWidget.tsx
@@ -5,27 +5,27 @@ import { useSpring, config, animated, a } from "@react-spring/three";
 import { motion, useScroll, useTransform, useMotionValue, useVelocity, useAnimationFrame } from "framer-motion";
 import React from "react";
 import GalleryCard from "./GalleryCard";
-import { getListCards } from "./helpers";
+import { getListCards, prepareCameraConfig } from "./helpers";
 
 
 const GalleryWidget = (props: any) => {
   const ref = useRef(null);
+
+  const [cameraConfig, setSetCameraConfig] = useState(prepareCameraConfig(null));
+
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start center", "center start"],
   });
-  const cameraX = useTransform(scrollYProgress, [0, 100], [0, 0.05]);
-  const cameraConfig: any = {
-    position: [0, 1, 0],
-    rotation: [0.2, 0, 0],
-  };
+  // const cameraX = useTransform(scrollYProgress, [0, 100], [0, 0.05]);
+  scrollYProgress.onChange((e) =>   setSetCameraConfig(prepareCameraConfig(e)))
 
   return (
     <section className="gallery-section mb-5 pb-5" ref={ref}>
       <Canvas className="canvas">
         <ambientLight />
         <spotLight position={[0, 5, 10]} penumbra={1} castShadow />
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         <perspectiveCamera {...cameraConfig}>
           <group>
             {getListCards().map((card: any, index: number) => (
--- src/components/gallery/helpers.ts
+++ src/components/gallery/helpers.ts
@@ -1,4 +1,4 @@
-import { Vector3 } from "@react-three/fiber";
+import { Object3DNode, Vector3 } from "@react-three/fiber";
 import { Euler } from "three";
 import ListCards from "./data/listCards.json";
 import { CardData, Vector3Numbers } from "./types";
@@ -15,10 +15,10 @@ const stepY = 1.5;
 const calcXForId = (idInRow: number) => minX + (idInRow - 1) * stepX;
 const calcYForId = (rowIndex: number) => minY + rowIndex * stepY;
 
-const calcPositionFromId = (id: number): Vector3 | null => {  
-  const x = calcXForId((id % cardsInRow) == 0 ? cardsInRow : (id % cardsInRow));
-  const y = calcYForId(Math.trunc((id -1) / cardsInRow));
-  return  [x, y, 3]
+const calcPositionFromId = (id: number): Vector3 | null => {
+  const x = calcXForId(id % cardsInRow == 0 ? cardsInRow : id % cardsInRow);
+  const y = calcYForId(Math.trunc((id - 1) / cardsInRow));
+  return [x, y, 3];
 };
 
 // generates block of cards and trying to use data from json if id matched
@@ -31,33 +31,43 @@ const getListCards = (): CardData[] => {
     const id = index + 1;
     const foundCardData = listCardData.find((c: any) => c.id === id);
 
-    const position = foundCardData && foundCardData.position ? foundCardData.position as Vector3 : calcPositionFromId(id);
-    const rotation = foundCardData && foundCardData.rotation ? foundCardData.rotation as any : defRotation;
-    
-    if(position){
-        listCards.push({
-            id,
-            position,
-            rotation,
-            item: 1,
-          });
-        }
-    }
-    console.log(listCards);
+    const position = foundCardData && foundCardData.position ? (foundCardData.position as Vector3) : calcPositionFromId(id);
+    const rotation = foundCardData && foundCardData.rotation ? (foundCardData.rotation as any) : defRotation;
 
+    if (position) {
+      listCards.push({
+        id,
+        position,
+        rotation,
+        item: 1,
+      });
+    }
+  }
   return listCards;
 };
 
-
-const hoverCard = (cardData: CardData): CardData  => {
+const hoverCard = (cardData: CardData): CardData => {
   const numPosition = cardData.position as Vector3Numbers;
-  const position: Vector3 = [numPosition[0], numPosition[1] + 0.1, numPosition[2] + 0.2]
+  const position: Vector3 = [numPosition[0], numPosition[1] + 0.1, numPosition[2] + 0.2];
   const rotation = new Euler(-0.1, 0, 0);
   return {
     ...cardData,
     position,
-    rotation
-  }
+    rotation,
+  };
+};
+
+const prepareCameraConfig = (scrollYstate: number| null): any =>{
+    if(scrollYstate === null){
+        return {
+          position: [0, 1, 0],
+          rotation: [0.2, 0, 0],
+        }
+    }
+    return {
+      position: [0, 1, 0],
+      rotation: [0.2 + scrollYstate, 0, 0],
+    }
 }
 
-export { getListCards, hoverCard };
+export { getListCards, hoverCard, prepareCameraConfig };
