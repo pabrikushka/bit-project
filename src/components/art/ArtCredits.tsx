69d608215ae681b0d681bbe498604c2a48034c87
Sergei <zags1611@gmail.com>
2023-04-18 10:46:55 +1200

    XXXXX static text not a button

--- src/components/art/ArtCredits.tsx
+++ src/components/art/ArtCredits.tsx
@@ -19,14 +19,19 @@ const ArtCredits = (props: ArtCreditsProps) => {
       <dl className="artist-credit d-flex align-items-center justify-content-between px-2 px-xxl-3 py-2 mb-4 py-xl-3">
         <dt className="fw-400">Artist</dt>
         <dd className="mb-0">
-          <button className="nav-link btn-link bg-transparent border-0 p-0 d-flex align-items-center" onClick={() => setArtistForModalModal(visualArtist)}>
-            <span className="h4 mb-0">{artPiece.artReleased? visualArtist?.name : notReleasedArtist}</span>
-          </button>
+          {artPiece.artReleased ? (
+            <button
+              className="nav-link btn-link bg-transparent border-0 p-0 d-flex align-items-center"
+              onClick={() => setArtistForModalModal(visualArtist)}
+            >
+              <span className="h4 mb-0">{visualArtist?.name}</span>
+            </button>
+          ) : (
+            notReleasedArtist
+          )}
         </dd>
       </dl>
-
-     <StatsWidget artPiece={artPiece} />
-
+      <StatsWidget artPiece={artPiece} />
     </>
   );
 };
