9e782dadc53a335014a8448318483e2d7f1478bf
SergeiZag <zags1611@gmail.com>
2023-04-27 18:17:55 +1200

    Generic transit animator (#13)

--- src/shared/TransitAnimator.tsx
+++ src/shared/TransitAnimator.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 interface TransitAnimatorProps {
-  image: any,
+  image: string | null;
 }
 
 const TransitAnimator = (props: TransitAnimatorProps) => {
-
   const { image } = props;
 
   return (
@@ -25,86 +24,78 @@ const TransitAnimator = (props: TransitAnimatorProps) => {
           scale: 1,
         }}
         transition={{
-          duration: .6,
+          duration: 0.6,
           ease: "easeOut",
           scale: {
-            duration: 1
-          }
+            duration: 1,
+          },
         }}
       >
-        <Container className='px-xl-5'>
+        <Container className="px-xl-5">
           <Row>
             <Col xs={12}>
-              <motion.div
-                className="animator-holder"
-              >
-                <motion.img
-                  className="animator-img"
-                  src={image}
-                />
-                <motion.div
-                  className="animator-curtains"
-                >
+              <motion.div className="animator-holder">
+                {image ? (
+                  <motion.img className="animator-img" src={image} />
+                ) : (
+                  <div className="bg-dark vw-100 vh-100" style={{ opacity: 0.9 }}></div>
+                )}
+                <motion.div className="animator-curtains">
                   <motion.div
                     className="animator-curtain"
                     exit={{
-                      y: '-100%'
+                      y: "-100%",
                     }}
                     transition={{
-                      delay: .5,
-                      duration: .3,
-                      ease: "easeOut"
+                      delay: 0.5,
+                      duration: 0.3,
+                      ease: "easeOut",
                     }}
-                  >
-                  </motion.div>
+                  ></motion.div>
                   <motion.div
                     className="animator-curtain"
                     exit={{
-                      y: '-100%'
+                      y: "-100%",
                     }}
                     transition={{
-                      delay: .3,
-                      duration: .3,
-                      ease: "easeOut"
+                      delay: 0.3,
+                      duration: 0.3,
+                      ease: "easeOut",
                     }}
-                  >
-                  </motion.div>
+                  ></motion.div>
                   <motion.div
                     className="animator-curtain"
                     exit={{
-                      y: '-100%'
+                      y: "-100%",
                     }}
                     transition={{
-                      delay: .1,
-                      duration: .3,
-                      ease: "easeOut"
+                      delay: 0.1,
+                      duration: 0.3,
+                      ease: "easeOut",
                     }}
-                  >
-                  </motion.div>
+                  ></motion.div>
                   <motion.div
                     className="animator-curtain"
                     exit={{
-                      y: '-100%'
+                      y: "-100%",
                     }}
                     transition={{
-                      delay: .3,
-                      duration: .3,
-                      ease: "easeOut"
+                      delay: 0.3,
+                      duration: 0.3,
+                      ease: "easeOut",
                     }}
-                  >
-                  </motion.div>
+                  ></motion.div>
                   <motion.div
                     className="animator-curtain"
                     exit={{
-                      y: '-100%'
+                      y: "-100%",
                     }}
                     transition={{
-                      delay: .5,
-                      duration: .3,
-                      ease: "easeOut"
+                      delay: 0.5,
+                      duration: 0.3,
+                      ease: "easeOut",
                     }}
-                  >
-                  </motion.div>
+                  ></motion.div>
                 </motion.div>
               </motion.div>
             </Col>
@@ -112,7 +103,7 @@ const TransitAnimator = (props: TransitAnimatorProps) => {
         </Container>
       </motion.div>
     </>
-  )
+  );
 };
 
 export default TransitAnimator;
