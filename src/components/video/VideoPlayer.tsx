33a516f72bff2a3d7956a86f94d8996cfb5ac500
Marlon Parra <marlonspr17@gmail.com>
2023-07-05 23:26:49 +1200

    ui fix moda and animation
    modal
    view more section

---
+++ src/components/video/VideoPlayer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css'
+
+interface VideoPlayerProps {
+  video: string;
+}
+
+const VideoPlayer = (props: VideoPlayerProps) => {
+  return (
+    <Video autoPlay loop>
+      <source src={props.video} type='video/mp4'/>
+    </Video>
+  )
+}
+
+export default VideoPlayer
