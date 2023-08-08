7a4b4f410e25c8fa57b8e9aa2a56fae431c3cd65
Marlon Parra <marlonspr17@gmail.com>
2023-08-08 13:33:49 +1200

    ui-fixes
    arrow animation
    and deleting knowledge from paralax text

--- src/assets/icons/arrowMore.js
+++ src/assets/icons/arrowMore.js
@@ -1,19 +1,19 @@
 const ArrowMore = () => {
   return (
-    <svg width='112' height='66' viewBox='0 0 112 66' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg width='112' height='112' viewBox='0 0 112 112' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <g clip-path='url(#clip0_10_10)'>
         <path
-          d='M18.1127 29.5978L14.7375 32.9871L47.5878 66L50.9887 62.5849L23.9098 35.3932L112 35.3932L112 30.5809L23.9098 30.581L50.9887 3.38926L47.5878 -2.83831e-06L18.1127 29.5978Z'
+          d='M18.1127 52.5978L14.7375 55.9871L47.5878 89L50.9887 85.5849L23.9098 58.3932L112 58.3932L112 53.581L23.9098 53.581L50.9887 26.3893L47.5878 23L18.1127 52.5978Z'
           fill='white'
         />
         <path
-          d='M32.8503 66L36.2512 62.5849L6.77618 32.9871L36.2512 3.38926L32.8502 -5.42694e-07L3.37521 29.5978L1.39131 31.59L3.93767e-06 32.9871L32.8503 66Z'
+          d='M32.8503 89L36.2512 85.5849L6.77618 55.9871L36.2512 26.3893L32.8502 23L3.37521 52.5978L1.39131 54.59L3.93767e-06 55.9871L32.8503 89Z'
           fill='white'
         />
       </g>
       <defs>
         <clipPath id='clip0_10_10'>
-          <rect width='112' height='66' fill='white' transform='translate(112 66) rotate(180)' />
+          <rect width='112' height='112' fill='white' transform='translate(112 112) rotate(180)' />
         </clipPath>
       </defs>
     </svg>
